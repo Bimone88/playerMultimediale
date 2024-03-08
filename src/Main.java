7e34ab7922944d1ddc69fba0e7e283e47250adde
Simone Manca <simone.manca.98@gmail.com>
2024-03-08 12:41:33 +0100

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-import multimedia.*; // per impoortarmi TUTTE le clssi
-
+import multimedia.*; // importo TUTTE le clssi
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ElementoMultimediale[] elementi = new ElementoMultimediale[5];
+        ElementoMultimediale[] elementi = new ElementoMultimediale[5]; // Creo un array di ElementoMultimediale
 
         // Creazione degli elementi multimediali
         for (int i = 0; i < elementi.length; i++) {
@@ -18,19 +17,19 @@ public class Main {
                 case 1:
                     System.out.println("Inserisci la luminosità: ");
                     int luminosita = Integer.parseInt(scanner.nextLine());
-                    elementi[i] = new Immagine(titolo, luminosita);
+                    elementi[i] = new Immagine(titolo, luminosita); // creazione istanza di IMMAGINE che metto nell'array
                     break;
                 case 2:
                     System.out.println("Inserisci il volume: ");
                     int volume = Integer.parseInt(scanner.nextLine());
-                    elementi[i] = new MiaRegistrazioneAudio(titolo, volume);
+                    elementi[i] = new MiaRegistrazioneAudio(titolo, volume); // Creazione istanza di "MiaRegistrazioneAudio" che metto nell'array
                     break;
                 case 3:
                     System.out.println("Inserisci la luminosità: ");
                     int luminositaVideo = Integer.parseInt(scanner.nextLine());
                     System.out.println("Inserisci il volume: ");
                     int volumeVideo = Integer.parseInt(scanner.nextLine());
-                    elementi[i] = new Video(titolo, luminositaVideo, volumeVideo);
+                    elementi[i] = new Video(titolo, luminositaVideo, volumeVideo); // Creazione istanxa di VIDEO e la metto nell'array
                     break;
                 default:
                     System.out.println("Tipo non valido.");
@@ -43,11 +42,19 @@ public class Main {
             System.out.println("Scegli quale elemento eseguire (da 1 a 5, 0 per terminare): ");
             scelta = Integer.parseInt(scanner.nextLine());
             if (scelta >= 1 && scelta <= 5) {
-                ElementoMultimediale elemento = elementi[scelta - 1];
+                ElementoMultimediale elemento = elementi[scelta - 1]; // Prendiamo l'elemento multimediale dall'array in base alla scelta dell'utente
+
+                // elementi e azioni
                 if (elemento instanceof Immagine) {
-                    ((Immagine) elemento).show();
-                } else if (elemento instanceof Riproducibile) {
-                    ((Riproducibile) elemento).play();
+                    ((Immagine) elemento).show(); // METODO show su IMMAGINE, abbastanza autoesplicativo :)
+                } else if (elemento instanceof RegistrazioneAudio) {
+                    RegistrazioneAudio audio = (RegistrazioneAudio) elemento;
+                    audio.play(); // METODO play su AUDIIO
+                    // chiedo all'utente su richiesya per alzare e abbassare il volume
+                } else if (elemento instanceof Video) {
+                    Video video = (Video) elemento;
+                    video.play(); //  METODO play fatto su VIDEO
+                    // Qua richiedo all'utente se buole abbassare o alzare il volume
                 }
             }
         } while (scelta != 0);
@@ -55,5 +62,6 @@ public class Main {
         scanner.close();
     }
 }
+
 
 
